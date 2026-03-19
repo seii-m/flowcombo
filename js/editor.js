5780f02debabdb1528a1dc499d8579756b73940d
Seii M <11038857+seii-m@users.noreply.github.com>
2026-03-19 21:52:11 +0900

    Add shape addition and JSON export/import handlers

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,20 @@
+document.getElementById("add-rect").onclick = () => {
+  console.log("四角を追加");
+};
+
+document.getElementById("add-circle").onclick = () => {
+  console.log("丸を追加");
+};
+
+document.getElementById("add-diamond").onclick = () => {
+  console.log("菱形を追加");
+};
+
+document.getElementById("export-json").onclick = () => {
+  console.log("Export");
+};
+
+document.getElementById("import-json").onclick = () => {
+  console.log("Import");
+};
 
